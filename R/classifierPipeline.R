f17750e38f6f48f463a5bbc7ce90ab88fb8689c1
Carlos Ruiz <yocra3@gmail.com>
2017-03-23 14:16:33 +0100

    Stop using lda to classify samples --> Use top score

--- R/classifierPipeline.R
+++ R/classifierPipeline.R
@@ -6,7 +6,6 @@
 #' @param SNPsR2 Vector with the R2 of the SNPs of the region
 #' @param hetRefs Vector with the heterozygote form of the SNP in the inversion
 #' @param Refs List with the allele frequencies in the references
-#' @param ldamodel LDAmodel used to classify individuals
 #' @param R2 Vector with the R2 between the SNPs and the inversion status
 #' @param mc.cores Numeric with the number of cores used in the computation
 #' @param verbose Should message be shown?
@@ -15,7 +14,7 @@
 #' \item{class: Factor with the classification of the individuals}
 #' \item{scores: Matrix with the simmilarity scores of the individuals}
 #' }
-classifierPipeline <- function(SNPlist, SNPsR2, hetRefs, Refs, ldamodel, R2 = 0.3,
+classifierPipeline <- function(SNPlist, SNPsR2, hetRefs, Refs, R2 = 0.3,
                                mc.cores = 1, verbose = FALSE){
   if (!all(c("map", "genotypes") %in% names(SNPlist))){
     stop("SNPlist must contain map and genotypes elements")
@@ -63,7 +62,7 @@ classifierPipeline <- function(SNPlist, SNPsR2, hetRefs, Refs, ldamodel, R2 = 0.
     message("Computing scores")
   }
   classifScore <- classifSNPspar(genos = genos, R2 = SNPsR2, refs = Refs, mc.cores = mc.cores)
-  inv <- getInvStatus(scores = classifScore, ldamodel = ldamodel)
+  inv <- getInvStatus(scores = classifScore)
   res <- new("SNPfieRes", classification = inv$class, certainty = inv$certainty,
              scores = t(classifScore))
   res
--- R/getInvStatus.R
+++ R/getInvStatus.R
@@ -4,15 +4,13 @@
 #' model.
 #'
 #' @param scores Matrix of similarity scores (from classifSNPspar)
-#' @param ldamodel LDAmodel used to classify individuals
-#' @param uncertainy.thres Numeric with the uncertainty threshold. If the maximum certainty of belonging
 #' to a group is smaller than this parameter, the individual is classified as NA.
 #' @return Factor with the inversion classification of the samples
-getInvStatus <- function(scores, ldamodel) {
-  pred <- MASS:::predict.lda(ldamodel, data.frame(t(scores)))
-  class <- pred$class
-  names(class) <- colnames(scores)
-  certainty <- apply(pred$posterior, 1, max)
-  res <- list(class = class, certainty = certainty)
-  return(res)
+getInvStatus <- function(scores) {
+  scores <- t(scores)
+  class <- factor(colnames(scores)[max.col(scores)], levels = colnames(scores))
+  names(class) <- rownames(scores)
+  posterior <- scores/rowSums(scores)
+  certainty <- apply(posterior, 1, max)
+  return(list(class = class, certainty = certainty))
 }
