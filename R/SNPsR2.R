6866e12c1b78c73c8b29c15f007b35bc60ba957b
Carlos Ruiz <yocra3@gmail.com>
2018-06-15 16:04:40 +0200

    Include four main inversions

--- R/SNPsR2.R
+++ R/SNPsR2.R
@@ -5,6 +5,6 @@
 #' of 1000 Genomes Phase 3 data. Real inversion status have been estimated using
 #' invClust.
 #'
-#' @format List of numeric vectors for 6 inversions (inv8p23.1, inv17q21.31, inv7p11.2
+#' @format List of numeric vectors for four inversions (inv8p23.1, inv17q21.31, inv7p11.2
 #' and invXq13.2).
 "SNPsR2"
